3350dbce9bcfee8bb763d6b30623cdb4cc12ec81
Alex Miranda <alex_miranda@outlook.com.br>
2022-05-06 07:28:31 -0300

    Inicio do exercicio 2 vetores 10 posições

---
+++ src/atividades/DoisVetores10Posicoes.java
@@ -0,0 +1,17 @@
+//8. Escreva um programa em Java que leia dois vetores de 10 posições e faça a multiplicação dos
+//elementos de mesmo índice, colocando o resultado em um terceiro vetor. Mostre o vetor resultante.
+
+package atividades;
+
+public class DoisVetores10Posicoes {
+
+	public static void main(String[] args) {
+		
+		int v1[] = {1,2,3,4,5,6,7,8,9,10};
+		int v2[] = {11,22,33,44,55,66,77,88,99,111};
+
+		
+
+	}
+
+}
