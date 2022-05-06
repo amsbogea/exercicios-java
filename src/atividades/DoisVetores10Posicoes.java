8920c473ad2ec6f02ef641d320f5c68dc1772ef8
Alex Miranda <alex_miranda@outlook.com.br>
2022-05-06 11:44:54 -0300

    Atividade concluída 2 vetores multiplicar

--- src/atividades/DoisVetores10Posicoes.java
+++ src/atividades/DoisVetores10Posicoes.java
@@ -1,17 +1,20 @@
-//8. Escreva um programa em Java que leia dois vetores de 10 posições e faça a multiplicação dos
-//elementos de mesmo índice, colocando o resultado em um terceiro vetor. Mostre o vetor resultante.
+//8. Escreva um programa em Java que leia dois vetores de 10 posições e 
+//faça a multiplicação dos elementos de mesmo índice, 
+//colocando o resultado em um terceiro vetor. Mostre o vetor resultante.
 
 package atividades;
 
 public class DoisVetores10Posicoes {
 
 	public static void main(String[] args) {
-		
-		int v1[] = {1,2,3,4,5,6,7,8,9,10};
-		int v2[] = {11,22,33,44,55,66,77,88,99,111};
 
-		
+		int v1[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+		int v2[] = { 11, 22, 33, 44, 55, 66, 77, 88, 99, 111 };
+		int i, j;
 
+		for (i = 0; i < v1.length; i++) {
+			System.out.println(v1[i] * v2[i]);
+
+		}
 	}
-
 }
